9bd75949f8ab768b6d5e9fe4777b8ea33dd484d4
moderateblue <127915442+moderateblue@users.noreply.github.com>
2023-03-22 19:09:02 -0700

    Create Main.java

---
+++ minesweeper/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    Mine minesweeper = new Mine();
+    
+    //System.out.print(minesweeper);
+    
+    System.out.println();
+
+    minesweeper.gameOn();
+    
+  }
+}
